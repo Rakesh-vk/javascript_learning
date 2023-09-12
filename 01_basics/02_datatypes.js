07dd334bbf59210dc7a5d8a7a86d41753d211777
Rakesh Kuratti <83536408+Rakesh-vk@users.noreply.github.com>
2023-09-12 20:04:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // this is used to treat all the javascript as newer version
+
+//number
+let num=123;
+//string =>"" or ''
+let str="hello"
+//boolean =>true/false
+let boo=true
+//null // the typeOf null is object
+let a=null
+// undefined is e=when we create a variable but dosen't assign a value;
+let z;
+// the typeOf undefined is undefined.
+// bigInt
